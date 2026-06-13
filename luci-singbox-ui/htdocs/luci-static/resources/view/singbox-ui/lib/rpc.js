ad0c5a9e6f6d672b54e8690f8850c36449f99a04
Jyn <iamnotjyn@gmail.com>
2026-06-14 04:19:16 +0700

    feat(dashboard): rpc wrappers for clash_delay, sub_status; refresh gains name

--- luci-singbox-ui/htdocs/luci-static/resources/view/singbox-ui/lib/rpc.js
+++ luci-singbox-ui/htdocs/luci-static/resources/view/singbox-ui/lib/rpc.js
@@ -2,7 +2,8 @@
 'require rpc';
 
 return L.Class.extend({
-    callRefresh:    rpc.declare({ object: 'singbox-ui', method: 'refresh',     params: [ 'what' ] }),
+    callRefresh:     rpc.declare({ object: 'singbox-ui', method: 'refresh',
+                                   params: [ 'what', 'name' ] }),
     callRestart:    rpc.declare({ object: 'singbox-ui', method: 'restart' }),
     callStatus:     rpc.declare({ object: 'singbox-ui', method: 'status' }),
     callReadConfig: rpc.declare({ object: 'singbox-ui', method: 'read_config' }),
@@ -10,6 +11,9 @@ return L.Class.extend({
                                    params: [ 'path' ] }),
     callClashMutate: rpc.declare({ object: 'singbox-ui', method: 'clash_mutate',
                                    params: [ 'method', 'path', 'body' ] }),
+    callClashDelay:  rpc.declare({ object: 'singbox-ui', method: 'clash_delay',
+                                   params: [ 'name', 'url', 'timeout' ] }),
+    callSubStatus:   rpc.declare({ object: 'singbox-ui', method: 'sub_status' }),
     callExportSection: rpc.declare({ object: 'singbox-ui', method: 'export_section',
                                      params: [ 'kind', 'name' ] }),
     callPreviewConfig: rpc.declare({ object: 'singbox-ui', method: 'preview_config' }),
